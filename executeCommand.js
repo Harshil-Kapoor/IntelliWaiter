9cc2860c5f1bd4eb2ad780adc380db6edb4d2f2a
Hashil Kapoor <harshilkapoor@gmail.com>
2017-05-05 10:50:09 +0530

    Included README file.

--- executeCommand.js
+++
@@ -1,9 +0,0 @@
-module.exports = (ssh, command, dir) => {
-	return (callback) => {
-		ssh.execCommand(command, { cwd: dir })
-		.then((result) => {
-			console.log(result);
-		    callback(null);
-	  	});
-	}
-}
